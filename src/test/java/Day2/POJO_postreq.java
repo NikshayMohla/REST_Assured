5b6bffcc661e1e9151a197eac82ecbc006136ec7
Nikshay Mohla <nikshay.mohla@rx.local>
2026-05-25 17:36:15 +0530

    Day 2

---
+++ src/test/java/Day2/POJO_postreq.java
@@ -0,0 +1,44 @@
+package Day2;
+
+public class POJO_postreq {
+    String name;
+    String location;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String[] getCoursesArr() {
+        return CoursesArr;
+    }
+
+    public void setCoursesArr(String[] coursesArr) {
+        CoursesArr = coursesArr;
+    }
+
+    String phone;
+    String[] CoursesArr;
+
+
+
+}
--- src/test/java/Day2/WaysToCreatePostRequest.java
+++ src/test/java/Day2/WaysToCreatePostRequest.java
@@ -1,5 +1,7 @@
 package Day2;
 
+import com.google.gson.JsonObject;
+import org.json.JSONObject;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
@@ -8,20 +10,70 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+
 public class WaysToCreatePostRequest {
 
-    @Test
-    void testPostUsingHashmap() {
-        HashMap data = new HashMap<>();
-        data.put("name", "Nikshay");
-        data.put("location", "Chandigarh");
-        data.put("phone", "099887766554");
-        String courseArr[] = {"C", "C++"};
-        data.put("courses", courseArr);
+    //    @Test(priority = 1)
+//    void testPostUsingHashmap() {
+//        HashMap data = new HashMap<>();
+//        data.put("name", "Nikshay");
+//        data.put("location", "Chandigarh");
+//        data.put("phone", "099887766554");
+//        String courseArr[] = {"C", "C++"};
+//        data.put("courses", courseArr);
+//
+//        given()
+//                .contentType("application/json")
+//                .body(data)
+//                .when()
+//                .post("http://localhost:3000/students")
+//
+//                .then()
+//                .statusCode(201)
+//                .body("name", equalTo("Nikshay"))
+//                .body("location", equalTo("Chandigarh"))
+//                .body("phone", equalTo("099887766554"))
+//                .header("content-type", "application/json; charset=utf-8")
+//                .log().all();
+//    }
+//    @Test(priority = 2)
+//    void testPostUsingJsonLibrary() {
+//        JSONObject data = new JSONObject();
+//        data.put("name", "Nikshay");
+//        data.put("location", "Chandigarh");
+//        data.put("phone", "099887766554");
+//        String courseArr[] = {"C", "C++"};
+//        data.put("courses", courseArr);
+//
+//
+//        given()
+//                .contentType("application/json")
+//                .body(data.toString())
+//                .when()
+//                .post("http://localhost:3000/students")
+//
+//                .then()
+//                .statusCode(201)
+//                .body("name", equalTo("Nikshay"))
+//                .body("location", equalTo("Chandigarh"))
+//                .body("phone", equalTo("099887766554"))
+//                .header("content-type", "application/json; charset=utf-8")
+//                .log().all();
+//    }
+
+    @Test(priority = 2)
+    void testPostUsingPOJOClass() {//plain old java object
+        POJO_postreq data = new POJO_postreq();
+        data.setName("Nikshay");
+        data.setLocation("Mohali");
+        data.setPhone("2020202020");
+        String[] arr = {"C++", "C"};
+        data.setCoursesArr(arr);
+
 
         given()
                 .contentType("application/json")
-                .body(data)
+                .body(data.toString())
                 .when()
                 .post("http://localhost:3000/students")
 
@@ -30,7 +82,15 @@ public class WaysToCreatePostRequest {
                 .body("name", equalTo("Nikshay"))
                 .body("location", equalTo("Chandigarh"))
                 .body("phone", equalTo("099887766554"))
-                .header("content-type", "application/json")
+                .header("content-type", "application/json; charset=utf-8")
                 .log().all();
+    }
+
+    @Test(priority = 3)
+    void deleteStudent() {
+        given()
+                .when()
+                .delete("http://localhost:3000/students/11")
+                .then().statusCode(200);
     }
 }
